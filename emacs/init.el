b2e51d9679fca6e146b1ab3f62339c7ae69f5602
Panashe M. Fundira <fundirap@gmail.com>
2016-05-13 22:52:56 -0400

    move to the dark side (emacs)
    add emacs config to VC

---
+++ emacs/init.el
@@ -0,0 +1,8 @@
+(load-file "~/.emacs.d/sensible-defaults.el")
+(sensible-defaults/use-all-settings)
+(sensible-defaults/use-all-keybindings)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+(require 'evil)
+(evil-mode 1)
