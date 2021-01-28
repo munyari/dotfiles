9115fd05b03e7c8ef73dcd43ea6866d88bbbc667
Panashe M. Fundira <panashe@dropbox.com>
2021-01-28 11:00:39 -0500

    [nvim] Reinstate neovim-remote support

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -41,6 +41,12 @@ end)
 
 --TODO: section comments plugin?
 
+-- Unix Editor variables
+if fn.executable('nvr') then
+  vim.env.VISUAL = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+  vim.env.EDITOR = vim.env.VISUAL
+end
+
 ---------------------------BASIC OPTIONS----------------------------------
 --[[
  vim has different scopes for the basic options. Setting an option
