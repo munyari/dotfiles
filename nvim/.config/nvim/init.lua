68a437691c90029a1c3041484507961aeb0fb26d
Panashe Fundira <fundirap@gmail.com>
2021-02-06 19:58:01 -0500

    [nvim] fix a config error

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,8 +6,8 @@ local g = vim.g       -- a table to access global variables
 local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  execute 'packadd packer.nvim'
+  cmd('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  cmd 'packadd packer.nvim'
 end
 
 cmd 'packadd packer.nvim'
