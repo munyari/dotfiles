b5ee5e3ec85195661444bc8cd02733805847276b
Panashe Fundira <fundirap@gmail.com>
2021-02-09 00:41:33 -0500

    A bunch of nix tweaks

--- nixpkgs/.config/nixpkgs/home.nix
+++ nixpkgs/.config/nixpkgs/home.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 
+let
+  rSERVER = "~/src/server";
+in
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
@@ -19,59 +22,115 @@
   # changes in each release.
   home.stateVersion = "21.03";
 
-  home.packages = [
-    pkgs.bash
-    pkgs.bat      # upgraded cat, with syntax highlighting and built in pager
-    pkgs.black
-    pkgs.chicken
-    pkgs.ctags
-    pkgs.curl
-    pkgs.delta    # better diffs
-    pkgs.fd
-    pkgs.ffmpeg
-    pkgs.fzf
-    pkgs.gnupg
-    pkgs.gnused
-    pkgs.gnutar
-    pkgs.go
-    pkgs.gopls
-    pkgs.htop
-    pkgs.jq
-    pkgs.less
-    pkgs.mosh
-    pkgs.mpv
-    pkgs.neovim-remote
-    pkgs.nodejs
-    pkgs.pandoc
-    pkgs.python
-    pkgs.python3
-    pkgs.qemu # virual machine go brrr
-    pkgs.ripgrep
-    pkgs.ruby
-    pkgs.stack
-    pkgs.stow
-    pkgs.tmux
-    pkgs.tree
-    pkgs.wget
-    pkgs.which
-    pkgs.youtube-dl
-    pkgs.zsh
+  home.packages = with pkgs; [
+    bash
+    black
+    cachix
+    chicken
+    ctags
+    curl
+    delta    # better diffs
+    fd
+    ffmpeg
+    gnupg
+    gnused
+    gnutar
+    go
+    gopls
+    htop
+    jq
+    less
+    mosh
+    mpv
+    neovim-remote
+    nodejs
+    pandoc
+    python
+    python3
+    qemu # virual machine go brrr
+    ripgrep
+    ruby
+    stack
+    stow
+    tree
+    wget
+    which
+    youtube-dl
   ];
 
-  programs.emacs = {
+  programs.alacritty.enable = true;
+
+  programs.bat = {
     enable = true;
-    extraPackages = epkgs: [
-      epkgs.nix-mode
-      epkgs.magit
-    ];
+    config = {
+      map-syntax = "BUILD.in:Python";
+    };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    defaultCommand = "fd";
   };
 
   programs.neovim = {
     enable = true;
     package = pkgs.neovim-nightly;
     vimdiffAlias = true;
+    withPython3 = true;
+    extraConfig = import ./config/neovim.nix;
   };
-  xdg.configFile."nvim/init.lua".source = ~/dotfiles/nvim/.config/nvim/init.lua;
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    defaultKeymap = "emacs";
+    enableCompletion = true;
+    autocd = true;
+    cdpath = [
+      "."
+      "~"
+      rSERVER
+    ];
+
+    history = {
+      expireDuplicatesFirst = true;
+      extended = true; # save timestamps in history file
+      ignoreDups = true; # don't enter a line if it's already in history
+    };
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./config/zsh;
+        file = "p10k.zsh";
+      }
+    ];
+
+    initExtra = "source $HOME/.nix-profile/etc/profile.d/nix.sh";
+
+    sessionVariables = {
+      EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'";
+      VISUAL = "$EDITOR";
+      PATH = "/opt/dropbox-override/bin:$PATH";
+      # Set the default Less options.
+      # Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
+      # Remove -X and -F (exit if the content fits on one screen) to enable it.
+      LESS="-F -g -i -M -R -S -w -X -z-4 --RAW-CONTROL-CHARS";
+    };
+
+    shellGlobalAliases = {
+      n = "nvim";
+      g = "git";
+    };
+  };
+
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
       url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
