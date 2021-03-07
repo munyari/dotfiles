de47e05f6f96a58880cbccde318bd1129f16f7a7
Panashe M. Fundira <panashe@dropbox.com>
2021-03-06 19:42:22 -0500

    [nix] fix PATH format

--- nixpkgs/.config/nixpkgs/home.nix
+++ nixpkgs/.config/nixpkgs/home.nix
@@ -77,7 +77,8 @@ in {
       [ ]);
 
   # TODO: look at targets.darwin configuration
-  # TODO: make bat MANPAGER
+  home.sessionPath = [ "/opt/dropbox-override/bin" ];
+
   programs.bat = {
     enable = true;
     config = {
@@ -405,7 +406,6 @@ in {
     initExtra = import ./config/zsh/initExtra.nix;
 
     sessionVariables = {
-      PATH = "/opt/dropbox-override/bin:$PATH";
       # Set the default Less options.
       # Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
       # Remove -X and -F (exit if the content fits on one screen) to enable it.
