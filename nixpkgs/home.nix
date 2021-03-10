dd8df4de54437211b45bdebdd65b42521fdfaba5
Panashe M. Fundira <panashe@dropbox.com>
2021-03-10 10:22:46 -0500

    [nvim] add some gitgutter configuration

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -376,7 +376,20 @@ in {
         fugitive
         fzf-vim
         fzf-lsp-nvim
-        gitgutter
+        {
+          plugin = gitgutter;
+          config = ''
+            let g:gitgutter_sign_modified = 'Δ'
+            let g:gitgutter_sign_modified_removed = '∎'
+            let g:gitgutter_sign_removed = '⨯'
+            let g:gitgutter_sign_column_always = 1
+            nnoremap <unique> [g :GitGutterPrevHunk<cr>
+            nnoremap <unique> ]g :GitGutterNextHunk<cr>
+            nnoremap <unique> <leader>hs :GitGutterStageHunk<cr>
+            nnoremap <unique> <leader>hp :GitGutterPreviewHunk<cr>
+            nnoremap <unique> <leader>hu :GitGutterUndoHunk<cr>
+          '';
+        }
         nvim-lspconfig
         nvim-treesitter
         surround
