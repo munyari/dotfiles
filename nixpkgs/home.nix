bcc3032b26dcf502f0327be2d0b332e3b018674e
Panashe M. Fundira <panashe@dropbox.com>
2021-03-11 21:57:51 -0500

    [nvim] add fugitive keymappings

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -373,7 +373,18 @@ in {
         commentary
         deoplete-lsp
         deoplete-nvim
-        fugitive
+        {
+          plugin = fugitive;
+          config = ''
+            nnoremap <silent><leader>gb       :Gblame<cr>
+            nnoremap <silent><leader>gc       :Gcommit -v<cr>
+            nnoremap <silent><leader>gd       :Gdiff<cr>
+            nnoremap <silent><leader>gp       :Gpush<cr>
+            nnoremap <silent><leader>gr       :Gread<cr>
+            nnoremap <silent><leader>gs       :Gstatus<cr>
+            nnoremap <silent><leader>gw       :Gwrite<cr>
+          '';
+        }
         fzf-vim
         fzf-lsp-nvim
         {
